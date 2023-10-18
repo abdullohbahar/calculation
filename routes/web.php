dc6f5d1ccf5bfa3aac692dcd21dcb0cfc753b194
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-10-18 15:12:46 +0700

    create HR calculation form page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\GuestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/kalukasi-hr', [GuestController::class, 'hrCalculation'])->name('hr.calculation');
+Route::get('/kalukasi-bisnis', [GuestController::class, 'index']);
