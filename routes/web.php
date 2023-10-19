bf895a6ee6ebf5b9283f5fcaabbe83f5856de87a
Baharudin Abdulloh Mun'im <abdullohbahar@gmail.com>
2023-10-19 10:48:59 +0700

    add asset and create login admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardAdminController;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Guest\GuestController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +26,11 @@ Route::post('/hasil-kalkulasi-form-hr', [GuestController::class, 'resultCalculat
 
 Route::get('/kalukasi-bisnis', [GuestController::class, 'businessCalculation'])->name('business.calculation');
 Route::post('/hasil-kalkulasi-form-bisnis', [GuestController::class, 'resultCalculationBusinessForm'])->name('result.calculation.business.form');
+
+Route::get('/admin/login', [AuthController::class, 'index'])->name('admin.login');
+Route::post('/admin/auth', [AuthController::class, 'authenticate'])->name('auth');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardAdminController::class, 'index'])->name('admin.dashboard');
+});
